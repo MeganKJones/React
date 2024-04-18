ebf8acced71bdb5c3442753331db35589accf354
Megan Jones <megan.jones@mobilise.cloud>
2024-04-18 09:07:48 +0100

    Game over screen shows name

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,15 @@
 import {useState} from 'react'
-export default function Player({initialName, symbol, isActive}) {
+
+export default function Player({initialName, symbol, isActive, onChangeName}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick({editStatus}) {
         setIsEditing((editing) => !editing);
 
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handleChange(event) {
@@ -19,14 +23,13 @@ export default function Player({initialName, symbol, isActive}) {
     }
 
 
-
     return (
-        <li className={isActive ? "active": undefined}>
+        <li className={isActive ? "active" : undefined}>
             <span className='player'>
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>{isEditing ? 'Save':'Edit'}</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
